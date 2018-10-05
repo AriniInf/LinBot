76ba03082dab34977690234d735ce35b6f709225
AriniInf <36852214+AriniInf@users.noreply.github.com>
2018-10-05 20:55:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,8 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-    
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(sender)) 
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
+     
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text + '/n' + profile.display_name))
     
     
 import os
