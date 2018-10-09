e51c570f15b10f079d0e10ba63b5a17e70a841ac
AriniInf <36852214+AriniInf@users.noreply.github.com>
2018-10-09 19:31:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -132,6 +132,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputmhs(data[1],data[2],data[3])))
     elif(data[0]=='hapus'):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hapusmhs(data[1])))
+    elif(data[0]=='ganti'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updatemhs(data[1],data[2],data[3],data[4])))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
