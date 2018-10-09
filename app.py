0fcd589a12847caa64b8436ebb11d3f345bc5d33
AriniInf <36852214+AriniInf@users.noreply.github.com>
2018-10-09 17:24:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=carimhs(text)))
+    #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=carimhs(text)))
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text="masuk"))
     data=text.split('-')
     if(data[0]=='lihat'):
